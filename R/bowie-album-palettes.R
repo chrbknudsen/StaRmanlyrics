51355e8bab2729f084297ae091f6655390646797
Christian Knudsen <christian.knudsen@gmail.com>
2024-01-05 12:39:19 +0100

    initial commit

---
+++ R/bowie-album-palettes.R
@@ -0,0 +1,31 @@
+#' farver for bowies albums
+#'
+#'
+#'
+
+album_palettes <- lapply(list(
+  aladdin_sane = c("#BF8F9C", "#BF6B8A", "#733630", "#BF5050", "#F2F2F2"),    #  bowie
+  fearless     = c("#6E4823", "#976F34", "#CBA863", "#ECD59F", "#E1D4C2"),    # fra taylor
+  fearless_tv  = c("#624324", "#A47F45", "#CAA462", "#C5AA7C", "#EEDBA9"),    # fra taylor
+  speak_now    = c("#2E1924", "#6C3127", "#833C63", "#D1A0C7", "#F5E8E2"),    # fra taylor
+  speak_now_tv = c("#2A122C", "#4a2454", "#72325F", "#874886", "#96689A"),    # fra taylor
+  red          = c("#201F39", "#A91E47", "#7E6358", "#B0A49A", "#DDD8C9"),    # fra taylor
+  red_tv       = c("#400303", "#731803", "#967862", "#B38468", "#C7C5B6"),    # fra taylor
+  `1989`       = c("#5D4E5D", "#846578", "#92573C", "#C6B69C", "#D8D8CF"),    # fra taylor
+  `1989_tv`    = c("#487398", "#659BBB", "#8BB5D2", "#AFC5D4", "#E4DFD3"),    # fra taylor
+  reputation   = c("#2C2C2C", "#515151", "#5B5B5B", "#6E6E6E", "#B9B9B9"),    # fra taylor
+  lover        = c("#76BAE0", "#8C4F66", "#B8396B", "#EBBED3", "#FFF5CC"),    # fra taylor
+  folklore     = c("#3E3E3E", "#545454", "#5C5C5C", "#949494", "#EBEBEB"),    # fra taylor
+  evermore     = c("#160E10", "#421E18", "#D37F55", "#85796D", "#E0D9D7"),    # fra taylor
+  midnights    = c("#121D27", "#5A658B", "#6F86A2", "#85A7BA", "#AA9EB6")    # fra taylor
+), color_palette)
+
+
+# et bud på farver fra space oddity
+# "#62d1f5"
+# "#a8d5a0"
+# "#939b9d"
+# "#614f49"
+# "#0082bf"
+
+
