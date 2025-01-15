043ca6b5e729e4af5edc38527c8796e01ea8dda0
Christian B. Knudsen <cbk@kb.dk>
2025-01-15 10:55:10 +0100

    vi tror vi har løst opdelingsproblemet.

--- R/grabs_lyrics.R
+++ R/grabs_lyrics.R
@@ -11,6 +11,28 @@ page %>% html_nodes("body") %>%
   html_nodes("#application") %>%
   html_nodes("#lyrics-root") %>%
   html_nodes('[data-lyrics-container="true"]') %>%
-    html_text2(preserve_nbsp = T)
+  as.character() %>%
+  str_remove_all(pattern = "<a href.*?>") %>%
+  str_remove_all(pattern = "<\\a>") %>%
+  str_remove_all(pattern = "<span.*?>") %>%
+  str_remove_all(pattern = "<\\span>") %>%
+  paste0(collapse = "\n") %>%
+  read_html() %>%
+  html_text2(preserve_nbsp = TRUE) %>%
+  str_split("\\n")
+
+
+<span.*?>
+
+  clipr::write_clip()
+  str_remove_all(pattern = "")
+
+
+  # Vi laver noderne om til HTML-strenge
+  paste0(collapse = "\n") %>%
+#   str_replace_all("<br\\s*/?>", "\n") %>%
+
+
+
 
 
